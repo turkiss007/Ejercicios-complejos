12083dc2787c598ecb8f9832805ffd0771e11ec4
Marta Dorado <martadoradobugallo@gmail.com>
2024-11-13 11:21:11 +0100

    Create MatriculaFinalConMetodo.java
    Matrícula con método

---
+++ MatriculaFinalConMetodo.java
@@ -0,0 +1,108 @@
+package Practica_ejercicios;
+
+import java.util.Scanner;
+
+public class MatriculaFinalConMetodo {
+    private static char saltarVocal (int i){
+        switch (letra[i]) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                letra[i]++;
+                break;
+        }
+        return letra[i];
+    }
+    public static void main(String[] args) {
+        int contador = 0;
+        boolean fueraLimites = false;
+        @SuppressWarnings("resource")
+        Scanner leer = new Scanner(System.in);
+
+        char letra[] = new char[3];
+        int numero[] = new int[7];
+
+        do {
+            System.out.println("Introduzca matrícula: ");
+            String matricula = leer.nextLine().toUpperCase();
+
+            contador = 0;
+            fueraLimites = false;
+            try{
+                for (int i = 0; i < matricula.length(); i++) {
+                    char caracter = matricula.charAt(i);
+                    if (Character.isLetter(caracter)) {
+                        letra[i] = caracter;
+                        switch (letra[i]) {
+                            case 'A':
+                            case 'E':
+                            case 'I':
+                            case 'O':
+                            case 'U':
+                                contador++;
+                                break;
+                        }
+                        // System.out.println(i + " letra: " + letra[i]);
+                    } else {
+                        numero[i] = caracter - '0';
+                        // System.out.println(i + "numero: " + numero[i]);
+                    }
+                }
+            } catch(ArrayIndexOutOfBoundsException ex){
+                System.err.println("Matrícula mal introducida. ");
+                fueraLimites = true;
+            }
+            if (contador > 0) {
+                System.out.println("La matrícula contiene vocales.");
+            }
+        } while ((contador > 0)||(fueraLimites==true));
+
+        numero[6]++;
+        if (numero[6] == 10) {
+            numero[6] = 0; 
+
+            numero[5]++;
+            if (numero[5] == 10) {
+                numero[5] = 0;
+
+                numero[4]++;
+                if (numero[4] == 10) {
+                    numero[4] = 0;
+
+                    numero[3]++;
+                    if (numero[3] == 10) {
+                        numero[3] = 0;
+
+                        letra[2]++;
+                        saltarVocal(2);
+                        if (letra[2] == 91) {
+                            letra[2] = 'B';
+
+                            letra[1]++;
+                            saltarVocal(1);
+                            if (letra[1] == 91) {
+                                letra[1] = 'B';
+
+                                letra[0]++;
+                                saltarVocal(0);
+                                if (letra[0]== 91) {
+                                    letra[0]='B';
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        String matriculaFinal=String.valueOf(letra[0]) + String.valueOf(letra[1]) + String.valueOf(letra[2]) + String.valueOf(numero[3]) + String.valueOf(numero[4]) + String.valueOf(numero[5]) + String.valueOf(numero[6]);
+        if (matriculaFinal.equals("BBB0000")) {
+            System.out.println("No hay más matrículas :(");
+        } else {
+            System.out.println("La siguiente matrícula es " + matriculaFinal );
+        }
+    }
+}
